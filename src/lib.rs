859df81ac7ce52c2b7d0b5ddd54eca45e908941c
krishnakumar4a4 <krishna.thokala2010@gmail.com>
2025-04-19 17:09:24 +0530

    feat: introduce line reader

--- src/lib.rs
+++ src/lib.rs
@@ -17,90 +17,20 @@ use crate::WinInfFileParseError::{FileDoNotExist, FileOpenError, FileReadError};
 
 pub struct WinInfFile {
     pub sections: HashMap<String, InfSection>,
+}
+
+#[derive(Default)]
+struct LineReader {
     pub remaining_string: String,
     pub lines: Vec<String>
 }
 
-#[derive(Debug)]
-pub enum WinInfFileParseError {
-    FileDoNotExist(),
-    FileOpenError(Error),
-    FileReadError(),
-    ReadLineError(String),
-}
-
-impl WinInfFile {
-    pub fn parse(&mut self, file_path: PathBuf) -> Result<(), WinInfFileParseError> {
-        // check if file exists
-        if !file_path.exists() {
-            return Err(FileDoNotExist());
-        }
-
-        let mut f = File::open(file_path);
-        if f.as_ref().is_err() {
-            return Err(FileOpenError(f.err().unwrap()));
-        }
-        let mut f = f.unwrap();
-        let mut decoder = UTF_16LE.new_decoder();
-
-        let buf_size = 1024;
-        let mut bom_detected = false;
-        loop {
-            let mut buf: Vec<u8> = vec![0; buf_size];
-            let read_count = f.read(&mut buf);
-            if read_count.is_err() {
-                return Err(FileReadError())
-            }
-            let read_count = read_count.unwrap();
-            if read_count <= 0 {
-                println!("bytes read: {}", read_count);
-                break;
-            }
-
-            // This properly prints the BOM value for the first buf
-            // Bom data: (Encoding { UTF-16LE }, 2)
-            let bom = Encoding::for_bom(&buf[..read_count]);
-            if let Some(b) = bom {
-                println!("Bom data: {b:?}");
-                bom_detected = true;
-            }
-
-            if bom_detected {
-                // This works perfectly for UTF16 LE
-                // Ref: https://learn.microsoft.com/en-us/windows-hardware/drivers/display/general-unicode-requirement
-                let mut utf16_buf = vec![0; buf_size/2];
-                let res = decoder.decode_to_utf16(&buf[..read_count], &mut utf16_buf, read_count != buf_size);
-                println!("result: {res:?}");
-                println!("decoded chars: {:?}", String::from_utf16_lossy(&utf16_buf[..res.2]));
-                if let Err(e) = self.read_line(&String::from_utf16_lossy(&utf16_buf[..res.2])) {
-                    return Err(WinInfFileParseError::ReadLineError(e.to_string()));
-                }
-            } else {
-                // This works if the file is UTF-8
-                println!("decoded chars: {:?}", String::from_utf8_lossy(&buf[..read_count]));
-                if let Err(e) = self.read_line(&String::from_utf8_lossy(&buf[..read_count]).to_string()) {
-                    return Err(WinInfFileParseError::ReadLineError(e.to_string()));
-                }
-            }
-        }
-
-        if !self.remaining_string.is_empty() {
-            self.lines.push(self.remaining_string.clone());
-        }
-
-        println!("total lines: {}", self.lines.len());
-        for line in self.lines.iter() {
-            println!(">> line: {}", line);
-        }
-
-        Ok(())
-    }
-
-    fn read_line(&mut self, input: &str) -> Result<(), &str>{
-        self.remaining_string.push_str(input);
+impl LineReader {
+    fn read_line(&mut self, line_part: &str) -> Result<(), &str>{
+        self.remaining_string.push_str(line_part);
         let mut found_cr = false;
         let mut new_line = String::from("");
-        for (i, c) in self.remaining_string.clone().chars().enumerate() {
+        for c in self.remaining_string.clone().chars() {
             // If LF did not follow CR, fail
             if found_cr && c != '\n' {
                 return Err("invalid crlf char, found \r but not \n immediately")
@@ -131,6 +61,88 @@ impl WinInfFile {
             new_line.push(c);
         }
         self.remaining_string = new_line;
+        Ok(())
+    }
+
+    fn finalize(&mut self) {
+        if !self.remaining_string.is_empty() {
+            self.lines.push(self.remaining_string.clone());
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum WinInfFileParseError {
+    FileDoNotExist(),
+    FileOpenError(Error),
+    FileReadError(),
+    ReadLineError(String),
+}
+
+impl WinInfFile {
+    pub fn parse(&mut self, file_path: PathBuf) -> Result<(), WinInfFileParseError> {
+        // check if file exists
+        if !file_path.exists() {
+            return Err(FileDoNotExist());
+        }
+
+        let mut f = File::open(file_path);
+        if f.as_ref().is_err() {
+            return Err(FileOpenError(f.err().unwrap()));
+        }
+        let mut f = f.unwrap();
+        let mut decoder = UTF_16LE.new_decoder();
+
+        let mut line_reader = LineReader::default();
+
+        let buf_size = 1024;
+        let mut bom_detected = false;
+        loop {
+            let mut buf: Vec<u8> = vec![0; buf_size];
+            let read_count = f.read(&mut buf);
+            if read_count.is_err() {
+                return Err(FileReadError())
+            }
+            let read_count = read_count.unwrap();
+            if read_count <= 0 {
+                println!("bytes read: {}", read_count);
+                break;
+            }
+
+            // This properly prints the BOM value for the first buf
+            // Bom data: (Encoding { UTF-16LE }, 2)
+            let bom = Encoding::for_bom(&buf[..read_count]);
+            if let Some(b) = bom {
+                println!("Bom data: {b:?}");
+                bom_detected = true;
+            }
+
+            if bom_detected {
+                // This works perfectly for UTF16 LE
+                // Ref: https://learn.microsoft.com/en-us/windows-hardware/drivers/display/general-unicode-requirement
+                let mut utf16_buf = vec![0; buf_size/2];
+                let res = decoder.decode_to_utf16(&buf[..read_count], &mut utf16_buf, read_count != buf_size);
+                println!("result: {res:?}");
+                println!("decoded chars: {:?}", String::from_utf16_lossy(&utf16_buf[..res.2]));
+                if let Err(e) = line_reader.read_line(&String::from_utf16_lossy(&utf16_buf[..res.2])) {
+                    return Err(WinInfFileParseError::ReadLineError(e.to_string()));
+                }
+            } else {
+                // This works if the file is UTF-8
+                println!("decoded chars: {:?}", String::from_utf8_lossy(&buf[..read_count]));
+                if let Err(e) = line_reader.read_line(&String::from_utf8_lossy(&buf[..read_count]).to_string()) {
+                    return Err(WinInfFileParseError::ReadLineError(e.to_string()));
+                }
+            }
+        }
+
+        line_reader.finalize();
+
+        println!("total lines: {}", line_reader.lines.len());
+        for line in line_reader.lines.iter() {
+            println!(">> line: {}", line);
+        }
+
         Ok(())
     }
 }
